c3bc717d5d5529f1fc0a3f6a31db10a2e266e46d
Selmen Arous <selmen.arous@gmail.com>
2022-11-21 04:21:28 +0100

    less build errors

--- setup.py
+++ setup.py
@@ -10,38 +10,13 @@ for line in open(os.path.join(setupdir, "requirements.txt"), encoding="UTF-8"):
         requirements.append(line)
 
 setup(
-    name="thonny-tunisiaschools",
-    version="1.0.0",
+    
     
     url="https://github.com/selmen2004/thonny-tunisiaschools",
-    author="Selmen Arous",
-    author_email= "selmen.arous@gmail.com",
-    license="GPL",
-    classifiers=[
-        "Environment :: MacOS X",
-        "Environment :: Win32 (MS Windows)",
-        "Environment :: X11 Applications",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: End Users/Desktop",
-        "License :: Freeware",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Natural Language :: English",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Education",
-        "Topic :: Software Development",
-    ],
-    keywords="PyQt QT Tunisia School Teaching Education",
+
+    
+    
+    
     platforms=["Windows", "macOS", "Linux"],
     python_requires=">=3.7",
     package_data={
